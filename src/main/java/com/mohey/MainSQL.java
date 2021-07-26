dfb64f93638e99bede5ac9619bc7196abec10175
Mohey El-Din Badr <MoheyEl-Din.Badr@outlook.com>
2021-07-26 14:15:30 +0200

    MEB: Adding SparkSQL

--- src/main/java/com/mohey/MainSQL.java
+++ src/main/java/com/mohey/MainSQL.java
@@ -2,9 +2,8 @@ package com.mohey;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.*;
+import static org.apache.spark.sql.functions.*;
 
 /**
  * @author Mohey El-Din Badr
@@ -19,14 +18,26 @@ public class MainSQL {
         SparkSession sparkSession = SparkSession.builder().
                 master("local[*]").appName("StartingSparkSQL").getOrCreate();
 
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> sparkSession.close()));
 
         Dataset<Row> dataset = sparkSession.read()
                 .option("header", true).csv("src/main/resources/exams/students.csv");
 
 
-        Dataset<Row> modernArtResults = dataset.filter("subject = 'Modern Art' ");
+        /*Dataset<Row> modernArtResults = dataset.filter("subject = 'Modern Art' AND year >= 2007");
+        modernArtResults.show();*/
 
+        /*Dataset<Row> modernArtResults = dataset.filter((FilterFunction<Row>) element -> element.getAs("subject").equals("Modern Art") && Integer.parseInt(element.getAs("year")) >= 2007);
+        modernArtResults.show();*/
 
-        sparkSession.close();
+        /*dataset.filter(col("subject").equalTo("Modern Art").and(col("year").geq(2007))).show();*/
+
+        dataset.createOrReplaceTempView("my_students_table");
+        //sparkSession.sql("SELECT * FROM my_students_table WHERE subject = 'Modern Art' AND year >= 2007").show();
+        //sparkSession.sql("SELECT max(score) as MAX FROM my_students_table WHERE subject = 'French'").show();
+        sparkSession.sql("SELECT DISTINCT year as years FROM my_students_table ORDER BY years DESC").show();
+
+        while(true);
+
     }
 }
